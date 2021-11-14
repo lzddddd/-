041b981d57228c44a82b2cd55ae2a695a5fae355
lzddddd <1916959422@qq.com>
2021-11-14 12:52:22 +0800

    second Commit: Login.vue demo

---
+++ src/views/login/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main">
+    <!-- 登录背景 -->
+    <img src="~assets/img/login/wave.png" alt="" class="wave">
+
+    <div class="bg">
+      <div class="main-bg">
+        <img src="~assets/img/login/login-bg.svg" alt=""> 
+      </div>
+    </div>
+  
+    <!-- 登录输入界面 -->
+    <div class="login-box">
+      <entry/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Entry from "./childComp/Entry";
+
+export default {
+  name: "Login",
+  components: {
+    Entry
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  width: 100vw;
+  height: 100vh;
+}
+
+.bg {
+  float: left;
+  width: 40%;
+  padding: 0 2rem;
+}
+
+.main-bg {
+  margin-top: 30%;
+  text-align: center;
+}
+
+.wave {
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100%;
+  z-index: -1;
+}
+
+.bg img {
+  width: 500px;
+}
+
+.login-box {
+  float: right;
+  width: 60%;
+  text-align: center;
+}
+</style>
---
+++ src/views/login/childComp/Entry.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="top-img">
+      <img src="~assets/img/login/avatar.svg" alt="">
+    </div>
+    <h2 class="welcome">欢迎</h2>
+    <div class="input-form">
+      <form action=""></form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+
+export default {
+  name: "Entry",
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style scoped>
+.top-img {
+  margin-top: 12%;
+}
+
+.top-img img {
+  width: 100px;
+  height: 100px;
+}
+
+.welcome {
+  font-size: 2.9rem;
+  text-transform: uppercase;
+  margin: 15px 0;
+  color: #999;
+}
+</style>
